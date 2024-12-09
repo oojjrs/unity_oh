c551ea3aef7ac94e13fb0716bed8ed4a9f56ee9a
oojjrs <oojjrs@gmail.com>
2024-12-10 08:53:53 +0900

    AutoDisabler 추가

---
+++ Assets/AutoDisabler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AutoDisabler : MonoBehaviour
+{
+    [SerializeField]
+    private float _seconds;
+
+    private void OnEnable()
+    {
+        _ = StartCoroutine(Func());
+
+        IEnumerator Func()
+        {
+            if (_seconds > 0)
+                yield return new WaitForSeconds(_seconds);
+            else
+                yield return default;
+
+            gameObject.SetActiveSafety(false);
+        }
+    }
+}
