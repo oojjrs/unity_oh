f00696eb938eb58a345baec5c7a2ef313d345bd6
oojjrs <oojjrs@blackstormcompany.com>
2025-01-21 16:12:55 +0900

    VectorExtensions 기능 추가

--- Assets/VectorExtensions.cs
+++ Assets/VectorExtensions.cs
@@ -9,9 +9,34 @@ namespace Assets
             return (v.x, v.z);
         }
 
-        public static Vector3 ToX0Z(this (float x, float y) tuple, float y = 0)
+        public static Vector2 Merge(this (float x, float y) tuple)
         {
-            return new(tuple.x, y, tuple.y);
+            return new(tuple.x, tuple.y);
+        }
+
+        public static Vector3 Merge(this (float x, float y, float z) tuple)
+        {
+            return new(tuple.x, tuple.y, tuple.z);
+        }
+
+        public static Vector3 MergeX0Z(this (float x, float z) tuple, float y = 0)
+        {
+            return new(tuple.x, y, tuple.z);
+        }
+
+        public static (float, float) Split(this Vector2 v)
+        {
+            return (v.x, v.y);
+        }
+
+        public static (float, float, float) Split(this Vector3 v)
+        {
+            return (v.x, v.y, v.z);
+        }
+
+        public static (float, float, float) SplitX0Z(this Vector2 v)
+        {
+            return (v.x, 0, v.y);
         }
     }
 }
