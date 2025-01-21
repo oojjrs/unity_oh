6672da9b4fe7f898ec93d36afe0e17405b5c3a3d
oojjrs <oojjrs@blackstormcompany.com>
2025-01-21 14:51:49 +0900

    VectorExtensions 추가

---
+++ Assets/VectorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public static class VectorExtensions
+    {
+        public static (float, float) GetXZ(this Vector3 v)
+        {
+            return (v.x, v.z);
+        }
+
+        public static Vector3 ToX0Z(this (float x, float y) tuple, float y = 0)
+        {
+            return new(tuple.x, y, tuple.y);
+        }
+    }
+}
