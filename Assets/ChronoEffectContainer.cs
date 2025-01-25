5510b00b2981c508528d27d59e20eb4f5cd7bcf0
oojjrs <oojjrs@gmail.com>
2025-01-25 15:19:49 +0900

    ChronoEffectPool과 ChronoEffectContainer 추가 (유니티 ObjectPool을 사용해보았다)

---
+++ Assets/ChronoEffectContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace oojjrs.oh
+{
+    public class ChronoEffectContainer
+    {
+        private ChronoEffectPool EffectPool { get; }
+        private Dictionary<Vector3, ChronoEffect> Values { get; }
+
+        public ChronoEffectContainer(ChronoEffect prefab, int chunkSize)
+        {
+            EffectPool = new(prefab, chunkSize);
+            Values = new();
+        }
+
+        public void Destroy()
+        {
+            EffectPool.Destroy();
+        }
+
+        public ChronoEffect GetOrCreate(Vector3 pos, Vector3? scale)
+        {
+            if (Values.TryGetValue(pos, out var value) == false)
+            {
+                value = EffectPool.Get();
+                value.transform.position = pos;
+
+                Values[pos] = value;
+            }
+
+            if (scale.HasValue)
+                value.transform.localScale = scale.Value;
+
+            return value;
+        }
+
+        public void ReleaseAll()
+        {
+            EffectPool.Release(Values.Values.ToArray());
+            Values.Clear();
+        }
+    }
+}
---
+++ Assets/ChronoEffectPool.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace oojjrs.oh
+{
+    public class ChronoEffectPool
+    {
+        private ObjectPool<ChronoEffect> Pool { get; }
+
+        public ChronoEffectPool(ChronoEffect prefab, int chunkSize)
+            : this(() =>
+            {
+                var e = Object.Instantiate(prefab);
+                e.SetActiveSafety(false);
+                return e;
+            }, obj => obj.DestroyObject(), chunkSize)
+        {
+        }
+
+        public ChronoEffectPool(System.Func<ChronoEffect> allocator, System.Action<ChronoEffect> deallocator, int chunkSize)
+        {
+            Pool = new(allocator, obj =>
+            {
+                obj.SetActiveSafety(true);
+            }, obj =>
+            {
+                obj.SetActiveSafety(false);
+            }, obj => deallocator(obj), defaultCapacity: chunkSize);
+        }
+
+        public void Destroy()
+        {
+            Pool.Clear();
+        }
+
+        public ChronoEffect Get()
+        {
+            return Pool.Get();
+        }
+
+        public void Release(params ChronoEffect[] values)
+        {
+            foreach (var value in values)
+                Pool.Release(value);
+        }
+    }
+}
