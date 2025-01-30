ec837cb2282014b26c7870df8fdeceb0b7361d8f
oojjrs <oojjrs@gmail.com>
2025-01-30 13:36:51 +0900

    TransformExtensions 추가

---
+++ Assets/TransformExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static void ForwardSafety(this Transform transform, Vector3 forward)
+    {
+        if (transform != default)
+        {
+            if (forward != Vector3.zero)
+                transform.forward = forward;
+        }
+    }
+
+    public static void PositionSafety(this Transform transform, Vector3 position)
+    {
+        if (transform != default)
+            transform.position = position;
+    }
+}
