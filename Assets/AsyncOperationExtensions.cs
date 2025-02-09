a25bf7afa61414a04898a8639056de10198ab497
oojjrs <oojjrs@gmail.com>
2025-02-09 18:48:43 +0900

    AsyncOperationExtensions 추가

---
+++ Assets/AsyncOperationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public static class AsyncOperationExtensions
+{
+    public static Task ToTask(this AsyncOperation asyncOperation)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        asyncOperation.completed += _ => tcs.SetResult(true);
+        return tcs.Task;
+    }
+}
