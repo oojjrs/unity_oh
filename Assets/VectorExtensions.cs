a29834f7787ab0dc45598bed5843176075d75ccd
oojjrs <oojjrs@blackstormcompany.com>
2025-02-12 12:40:09 +0900

    tuple 시리즈를 삭제하고 ToX0Z 표준 버전 추가

--- Assets/VectorExtensions.cs
+++ Assets/VectorExtensions.cs
@@ -1,45 +1,5 @@
 ﻿public static class VectorExtensions
 {
-    public static (float, float) GetXZ(this UnityEngine.Vector3 v)
-    {
-        return (v.x, v.z);
-    }
-
-    public static UnityEngine.Vector2 Merge(this (float x, float y) tuple)
-    {
-        return new(tuple.x, tuple.y);
-    }
-
-    public static UnityEngine.Vector3 Merge(this (float x, float y, float z) tuple)
-    {
-        return new(tuple.x, tuple.y, tuple.z);
-    }
-
-    public static UnityEngine.Vector3 MergeX0Z(this (float x, float z) tuple, float y = 0)
-    {
-        return new(tuple.x, y, tuple.z);
-    }
-
-    public static UnityEngine.Vector3 MergeX0Z(this UnityEngine.Vector2 v, float y = 0)
-    {
-        return new(v.x, y, v.y);
-    }
-
-    public static (float, float) Split(this UnityEngine.Vector2 v)
-    {
-        return (v.x, v.y);
-    }
-
-    public static (float, float, float) Split(this UnityEngine.Vector3 v)
-    {
-        return (v.x, v.y, v.z);
-    }
-
-    public static (float, float, float) SplitX0Z(this UnityEngine.Vector2 v)
-    {
-        return (v.x, 0, v.y);
-    }
-
     public static System.Numerics.Vector2 ToStandard(this UnityEngine.Vector2 v)
     {
         return new(v.x, v.y);
@@ -58,5 +18,15 @@
     public static UnityEngine.Vector3 ToUnity(this System.Numerics.Vector3 v)
     {
         return new(v.X, v.Y, v.Z);
+    }
+
+    public static System.Numerics.Vector3 ToX0Z(this System.Numerics.Vector2 v, float y = 0)
+    {
+        return new(v.X, y, v.Y);
+    }
+
+    public static UnityEngine.Vector3 ToX0Z(this UnityEngine.Vector2 v, float y = 0)
+    {
+        return new(v.x, y, v.y);
     }
 }
