95a8602a51124270eeefae8821b885e4afb72ba4
oojjrs <oojjrs@blackstormcompany.com>
2025-02-12 14:12:37 +0900

    LifeTime 추가

---
+++ Assets/LifeTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace oojjrs.oh
+{
+    public class LifeTime : MonoBehaviour
+    {
+        [SerializeField]
+        private float _seconds = 1;
+
+        private IEnumerator Start()
+        {
+            if (_seconds > 0)
+                yield return new ChronoWaitForSeconds(_seconds);
+
+            gameObject.Destroy();
+        }
+    }
+}
