140665a4e03166c700617d4b646aed9c284ac712
oojjrs <oojjrs@gmail.com>
2025-09-23 10:14:30 +0900

    AppEnvironmentInfo 추가

--- Assets/MyEnv.cs
+++ Assets/MyEnv.cs
@@ -2,6 +2,8 @@
 
 public static class MyEnv
 {
+    public static string AppEnvironmentInfo => Application.platform + "/" + Application.version + "/" + Application.unityVersion + "/" + SystemInfo.deviceModel + "/" + SystemInfo.operatingSystem;
+
     public static bool IsDevelopmentBuild()
     {
         return Application.isEditor || Debug.isDebugBuild;
