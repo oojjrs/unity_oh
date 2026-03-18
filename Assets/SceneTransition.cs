340c584a8000896f61dbb77ab2142d923eea9690
oojjrs <oojjrs@gmail.com>
2026-03-18 20:31:44 +0900

    SceneTransition을 초소형으로 시작해보았다.

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace oojjrs.oh
+{
+    [RequireComponent(typeof(SolidObject))]
+    public class SceneTransition : MonoBehaviour
+    {
+        public interface RequestInterface
+        {
+            string SceneName { get; }
+
+            void OnPostload();
+            void OnPreload();
+        }
+
+        public IEnumerator LoadAsync(RequestInterface request)
+        {
+            request.OnPreload();
+
+            if (SceneManager.GetActiveScene().name != request.SceneName)
+            {
+                var time = Time.time;
+                Debug.Log("LOAD BEGIN : " + request.SceneName);
+                {
+                    yield return SceneManager.LoadSceneAsync(request.SceneName);
+                }
+                Debug.Log($"LOAD END : {Time.time - time} seconds");
+            }
+
+            request.OnPostload();
+        }
+    }
+}
