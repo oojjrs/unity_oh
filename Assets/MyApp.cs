89d20e9ab22ce0093c10ffe088e53d637fa0aa52
oojjrs <oojjrs@gmail.com>
2026-03-21 11:10:09 +0900

    MyApp 추가

---
+++ Assets/MyApp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class MyApp
+{
+    public static void Quit()
+    {
+#if UNITY_EDITOR
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+#endif
+        {
+            Application.Quit();
+        }
+    }
+}
