54afffe104f5a519a4af23ce04b93ae979ecb685
oojjrs <oojjrs@gmail.com>
2026-04-21 20:28:23 +0900

    Animator 변수 Finder 추가

---
+++ Packages/src/AnimatorFinderFromVariable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class AnimatorFinderFromVariable : FinderT<Animator>
+{
+    [SerializeField]
+    private Animator _animator;
+
+    public override Animator Value { get => _animator; set => _ = value; }
+}
