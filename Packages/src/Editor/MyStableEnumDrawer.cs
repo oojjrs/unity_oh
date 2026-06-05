3942fd7282fcb4fac861536d631d220b538d6236
oojjrs <oojjrs@gmail.com>
2026-06-06 08:38:26 +0900

    StableEnum 드로어 개선

--- Packages/src/Editor/MyStableEnumDrawer.cs
+++ Packages/src/Editor/MyStableEnumDrawer.cs
@@ -8,23 +8,31 @@ public sealed class StableEnumDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        var attr = (MyStableEnumAttribute)attribute;
-
-        if (property.propertyType != SerializedPropertyType.String)
+        EditorGUI.BeginProperty(position, label, property);
         {
-            EditorGUI.LabelField(position, label.text, $"{nameof(MyStableEnumAttribute)} requires string field");
-            return;
+            var attr = (MyStableEnumAttribute)attribute;
+
+            if (property.propertyType != SerializedPropertyType.String)
+            {
+                EditorGUI.LabelField(position, label.text, $"{nameof(MyStableEnumAttribute)} requires string field");
+            }
+            else
+            {
+                var names = Enum.GetNames(attr.EnumType);
+                var index = Array.IndexOf(names, property.stringValue);
+
+                if (index < 0)
+                    index = 0;
+
+                EditorGUI.BeginChangeCheck();
+
+                var nextIndex = EditorGUI.Popup(position, label.text, index, names);
+
+                if (EditorGUI.EndChangeCheck())
+                    property.stringValue = names[nextIndex];
+            }
         }
-
-        var names = Enum.GetNames(attr.EnumType);
-        var index = Array.IndexOf(names, property.stringValue);
-
-        if (index < 0)
-            index = 0;
-
-        var nextIndex = EditorGUI.Popup(position, label.text, index, names);
-
-        property.stringValue = names[nextIndex];
+        EditorGUI.EndProperty();
     }
 }
 #endif
